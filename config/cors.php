4bbe9c8e439d1f9f1d1fd40cfcb6f80ed95d6448
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-11-07 16:16:24 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aperevolutionzone.com'],
+    'allowed_origins' => ['https://aperevolutionzone.com', 'https://www.aperevolutionzone.com'],
 
     'allowed_origins_patterns' => [],
 
